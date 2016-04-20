18f9e745d95f6d7dfad3bfc85ddffb401d2bdf4c
ibizamyholidays <jarroyo.lancersales@gmail.com>
2016-04-20 03:33:10 +0200

    Create constructor.js

---
+++ constructor.js
@@ -0,0 +1,69 @@
+$(document).ready(function() {
+    $('#example').DataTable({
+    "paging":   false,
+    "bPagination": false
+    });
+} );
+
+
+$(function () {
+
+    $('#container').highcharts('StockChart',{
+        chart: {
+		type: "line"
+	},
+	rangeSelector: {
+		enabled: false
+	},
+	xAxis: {
+		type: "datetime"
+	},
+	plotOptions:{
+            series:{
+                turboThreshold:10000,
+                lineWidth: 1
+            }
+    
+    },
+    series: dataset
+  });
+  
+   var chart = $('#container').highcharts(),
+        buttons = $('.choice');
+        
+    $('.choice').click(function () {
+        
+        buttons.each(function() {
+            var series;
+            var currentBox = $(this);
+            var isChecked = currentBox.is(':checked');
+            var wasChecked = currentBox.data("wasChecked");
+            var choice = currentBox.val();
+            $('#console').innerHTML = choice;
+
+            if(!wasChecked && isChecked)
+            {
+                series = chart.series[choice];
+                currentBox.data("series", series);
+                series.show();
+            }
+            
+            if(wasChecked && !isChecked)
+            {
+                series = currentBox.data("series");
+                series.hide();
+            }
+            
+            if(isChecked)
+            {
+                currentBox.data("wasChecked", true);
+            }
+            else
+            {
+                currentBox.data("wasChecked", false);
+            }
+        });
+        
+    });
+  
+});
